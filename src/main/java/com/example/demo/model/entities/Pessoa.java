a337b97ae8224014de27b47f0bafb38d2deeb8a7
brittoglauber <brittoglauber@gmail.com>
2023-08-16 16:08:42 -0300

    Criando classe pessoa

---
+++ src/main/java/com/example/demo/model/entities/Pessoa.java
@@ -0,0 +1,62 @@
+package com.example.demo.model.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_pessoa")
+public class Pessoa implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Date dataNascimento;
+    private String cpf;
+
+    public Pessoa() {
+
+    }
+
+    public Pessoa(Long id, String nome, Date dataNascimento, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
